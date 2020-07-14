f55a530a83387e2ca720d8ed38b62b3175f151d0
Greg <spam@ilseum.com>
2020-07-14 10:43:32 -0700

    Chapter 9 - End

--- chapter9/large_sizes.py
+++ chapter9/large_sizes.py
@@ -7,7 +7,8 @@ for folder, sub, files in os.walk(os.getcwd()):
         whole = folder + '/' + file
         try:
             size = os.path.getsize(whole)
-            if size > 500000000:
-                print(f"{whole} ......... {os.path.getsize(whole)}")
         except FileNotFoundError:
-            pass
+            continue
+
+        if size > 500000000:
+            print(f"{whole} ......... {size}")
